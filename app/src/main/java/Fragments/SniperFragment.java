27222829d6c017cb870cb24918f4cc7e0f5dd44f
zsezs <alireza1376@gmail.com>
2018-01-30 13:02:47 +0330

    ride shod too menu

---
+++ app/src/main/java/Fragments/SniperFragment.java
@@ -0,0 +1,26 @@
+package Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.coins.black.main_menu.R;
+
+
+
+public class SniperFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sniper, container, false);
+    }
+
+
+}
