cc15a3e447162aa450b90227ba99d8c98cccc407
Packdel <amd.amp1969@gmail.com>
2018-02-09 10:27:09 +0330

    commit35 : working on joining room

--- app/src/main/java/com/coins/black/main_menu/JoiningRoom.java
+++ app/src/main/java/com/coins/black/main_menu/JoiningRoom.java
@@ -1,20 +1,33 @@
 package com.coins.black.main_menu;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ProgressBar;
+
+import com.github.nkzawa.socketio.client.Socket;
 
 
 public class JoiningRoom extends AppCompatActivity {
 
-    Button startChat;
+    public Socket socket;
 
-    private ProgressBar spinner; //change
+    public static SharedPreferences save;
+
+    public static void log(String info){
+        Log.i("AAA Team :",info);}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joining_room);
+
+        // geting the socket from main activity
+        socket = MainActivity.socket;
+
+        // get the app's SharedPreferences
+        save = getPreferences(0);
     }
 }
